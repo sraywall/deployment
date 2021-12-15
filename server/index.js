17bbecd4079c6ab3e55125e4baa00717756c07e1
Stephen Wall <sraywall@gmail.com>
2021-12-15 14:58:08 -0700

    added css

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const app = express()
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'../index.html'))
 })
+app.get('/css', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../styles.css'))
+})
 
+// app.use(express.static(__dirname, '../'))
 const port = process.env.PORT || 4005
 
 app.listen(port,()=> console.log(`Avengers! Assemble.. on port ${port}.`))
